eb1bd1f29ddab14efdca99f14687be09267c74f2
tetrash <szyma.karol@gmail.com>
2020-03-14 18:23:54 +0100

    Update function

--- function/handler.js
+++ function/handler.js
@@ -3,7 +3,7 @@
 const wkhtmltopdf = require('wkhtmltopdf');
 const MemoryStream = require('memorystream');
 
-module.exports = async (context, callback) => {
+module.exports = async (event, context) => {
     const memStream = new MemoryStream();
     const html_utf8 = new Buffer(event.html_base64, 'base64').toString('utf8');
     wkhtmltopdf(html_utf8, event.options, (code, signal) => {
