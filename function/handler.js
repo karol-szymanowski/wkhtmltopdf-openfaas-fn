ae2f300e1ed96771251e6a35cb576f4e3caebdc2
tetrash <szyma.karol@gmail.com>
2020-03-14 18:50:38 +0100

    Fix body request

--- function/handler.js
+++ function/handler.js
@@ -5,8 +5,8 @@ const MemoryStream = require('memorystream');
 
 module.exports = async (event, context) => {
     const memStream = new MemoryStream();
-    const html_utf8 = new Buffer(event.html_base64, 'base64').toString('utf8');
-    wkhtmltopdf(html_utf8, event.options, (code, signal) => {
+    const html_utf8 = new Buffer(event.body.html_base64, 'base64').toString('utf8');
+    wkhtmltopdf(html_utf8, event.body.options, (code, signal) => {
         context.done(null, { pdf_base64: memStream.read().toString('base64') });
     }).pipe(memStream);
 }
