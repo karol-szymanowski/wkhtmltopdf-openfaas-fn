e1bd7836a492c743301fff01f152ccf5649af15d
tetrash <szyma.karol@gmail.com>
2020-03-14 19:24:50 +0100

    Final

--- function/handler.js
+++ function/handler.js
@@ -4,10 +4,10 @@ const wkhtmltopdf = require('wkhtmltopdf');
 const MemoryStream = require('memorystream');
 
 module.exports = async (event, callback) => {
-    callback(null, { event });
-    // const memStream = new MemoryStream();
-    // const html_utf8 = new Buffer(event.body.html_base64, 'base64').toString('utf8');
-    // wkhtmltopdf(html_utf8, event.body.options, (code, signal) => {
-    //     callback(null, { pdf_base64: memStream.read().toString('base64') });
-    // }).pipe(memStream);
+    const body = JSON.parse(event);
+    const memStream = new MemoryStream();
+    const html_utf8 = new Buffer(body.html_base64, 'base64').toString('utf8');
+    wkhtmltopdf(html_utf8, body.options, (code, signal) => {
+        callback(null, { pdf_base64: memStream.read().toString('base64') });
+    }).pipe(memStream);
 }
