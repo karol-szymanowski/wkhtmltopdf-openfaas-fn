58cfb2d70ab126ef9914cb35d3c44972e889ce61
tetrash <szyma.karol@gmail.com>
2020-03-14 19:10:47 +0100

    Fix body request

--- function/handler.js
+++ function/handler.js
@@ -3,11 +3,11 @@
 const wkhtmltopdf = require('wkhtmltopdf');
 const MemoryStream = require('memorystream');
 
-module.exports = async (event, context) => {
-    context.done(null, { event, context });
+module.exports = async (event, callback) => {
+    callback(null, { event, context });
     // const memStream = new MemoryStream();
     // const html_utf8 = new Buffer(event.body.html_base64, 'base64').toString('utf8');
     // wkhtmltopdf(html_utf8, event.body.options, (code, signal) => {
-    //     context.done(null, { pdf_base64: memStream.read().toString('base64') });
+    //     callback(null, { pdf_base64: memStream.read().toString('base64') });
     // }).pipe(memStream);
 }
